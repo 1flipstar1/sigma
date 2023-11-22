91cf80016099db7389710331a08c76cb45a9b8d7
Kucheriav <kuchborisovich@gmail.com>
2023-11-22 21:25:12 +0300

    compiled all into version 1.0

--- version 1.0.py
+++ version 1.0.py
@@ -2,11 +2,23 @@ import requests
 import pygame
 import sys
 import os
-from main import get_points_by_id_from_db
+import sqlite3
 
+
+DB_NAME = 'project.sqlite'
 MAP_FILE_NAME = 'map.png'
 pygame.init()
+
+
+def get_points_by_id_from_db(id):
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    cursor.execute(f'''SELECT rout FROM routes WHERE id = {id}''')
+    res = cursor.fetchone()
+    return res[0]
+
 def transform_from_dp_to_api(line):
+    print(line)
     line = line.replace(', ', ',')[:-1]
     line = line.split('; ')
     line = map(lambda x: x[2:], line)
@@ -15,21 +27,15 @@ def transform_from_dp_to_api(line):
     line = '~'.join(line)
     return line
 
-
 def get_map(points):
-    # map_request = 'http://static-maps.yandex.ru/1.x/?pt=36.242889,54.511964,pm2ywm50~36.242638,54.510594,pm2wtm50&spn=0.002,0.002&l=map'
     api_server = 'http://static-maps.yandex.ru/1.x/'
     params = {
-        #"ll": "36.242889,54.511964",
-        #"spn": "0.002,0.002",
         "l": "map",
         'size': '640,450',
-        'z': '15',
+        'z': '12',
         'pt': points
     }
     response = requests.get(api_server, params=params)
-
-
     if not response:
         print('Ошибка выполнения запроса:')
         print('Http статус:', response.status_code, '(', response.reason, ')')
@@ -39,9 +45,10 @@ def get_map(points):
 
 
 
-
+print('input number')
+n = int(input())
 screen = pygame.display.set_mode((640, 450))
-points = transform_from_dp_to_api(get_points_by_id_from_db(3))
+points = transform_from_dp_to_api(get_points_by_id_from_db(n))
 print(points)
 map_content = get_map(points)
 if not map_content:
