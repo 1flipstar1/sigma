f34428b684dcd8afb5c96e79c739e6cd7b67d845
0Valter0 <pavel.kondrashov2008@gmail.com>
2023-11-24 20:55:27 +0300

    version 1.0

--- version1_0.py
+++ version1_0.py
@@ -4,15 +4,9 @@ import sys
 import os
 import sqlite3
 
-
 DB_NAME = 'project.sqlite'
 MAP_FILE_NAME = 'map.png'
 pygame.init()
-
-
-
-
-
 
 def get_points_by_id_from_db(id):
     db = sqlite3.connect(DB_NAME)
@@ -36,6 +30,13 @@ def get_routes_info_by_type_from_db(type):
     res = cursor.fetchall()
     return res
 
+def get_info_by_type_from_db(type):
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    cursor.execute(f'''SELECT id, name  FROM routes WHERE type = "{type}"''')
+    res = cursor.fetchall()
+    return res
+
 def get_transport_types_from_db(matter):
     db = sqlite3.connect(DB_NAME)
     cursor = db.cursor()
@@ -44,11 +45,10 @@ def get_transport_types_from_db(matter):
     res = [x[0] for x in res]
     return res
 
-
 def transform_from_db_to_api(line):
     line = line.replace(', ', ',')[:-1]
     line = line.split('; ')
-    line = map(lambda x: x[2:], line)
+    line = map(lambda x: x[x.index(':') + 1:], line)
     line = map(lambda x: [x.split(',')[1], x.split(',')[0], 'flag'], line)
     line = map(lambda x: ','.join(x), line)
     line = '~'.join(line)
@@ -66,25 +66,43 @@ def get_map(points):
     if not response:
         print('Ошибка выполнения запроса:')
         print('Http статус:', response.status_code, '(', response.reason, ')')
+        print(points)
         return False
     return response.content
 
+print('Здравствуйте, вас привествует первый путеводитель по Калуге!')
 
+while True:
+    print('Выберите тематику:')
+    print(*get_matters_from_db())
+    matter = input()
+    types = get_transport_types_from_db(matter)
+    if types:
+        break
+    else:
+        print('Ой-ой, кажется вы ввели то-то неправильное!', 'Попытайтесь снова! :)', end='\n')
 
-print('Здравствуйте, вас привествует первый путеводитель по Калуге')
-print('Выберите тематику')
-print(*get_matters_from_db())
-matter = input()
-print('Выберите тип передвижения')
-print(*get_transport_types_from_db(matter))
-type = input()
-print('Вот варианты маршрутов. Выберите номер')
-res = get_routes_info_by_type_from_db(type)
-for i in res:
-    print(*i)
-n = input()
+while True:
+    print('Выберите тип передвижения:')
+    print(*types)
+    type = input()
+    res = get_routes_info_by_type_from_db(type)
+    if res:
+        break
+    else:
+        print('Ой-ой, кажется вы ввели то-то неправильное!', 'Попытайтесь снова! :)', end='\n')
+while True:
+    try:
+        print('Вот варианты маршрутов. Выберите номер:')
+        for i in res:
+            print(*i)
+        n = input()
+        points = get_points_by_id_from_db(n)
+        break
+    except:
+        print('Ой-ой, кажется вы ввели то-то неправильное!', 'Попытайтесь снова! :)', end='\n')
 screen = pygame.display.set_mode((640, 450))
-points = transform_from_db_to_api(get_points_by_id_from_db(n))
+points = transform_from_db_to_api(points)
 map_content = get_map(points)
 if not map_content:
     sys.exit(-1)
